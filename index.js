f9ddef48dcd587ec3f8b2e1ca92cbdeed9a963a0
Hao Wang <ge_nah@hotmail.com>
2019-07-20 17:04:05 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(array) {
+  const result = array.find(e =>
+    e.result == "W"
+  );
+  if(result) {
+    return result.year;
+  }else {
+    return undefined
+  }
+}
